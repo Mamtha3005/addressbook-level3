4f0f445c43a68776384216ab2980a1442e9efcc0
DESKTOP-Q2E8UPT\Mamtha <mamtha3005@gmail.com>
2016-09-13 21:04:08 -0700

    Did polyorphism of isMutating method. only to store data if mutated

--- src/seedu/addressbook/commands/IncorrectCommand.java
+++ src/seedu/addressbook/commands/IncorrectCommand.java
@@ -1,14 +1,14 @@
 package seedu.addressbook.commands;
 
-
 /**
- * Represents an incorrect command. Upon execution, produces some feedback to the user.
+ * Represents an incorrect command. Upon execution, produces some feedback to
+ * the user.
  */
-public class IncorrectCommand extends Command{
+public class IncorrectCommand extends Command {
 
     public final String feedbackToUser;
 
-    public IncorrectCommand(String feedbackToUser){
+    public IncorrectCommand(String feedbackToUser) {
         this.feedbackToUser = feedbackToUser;
     }
 
@@ -17,4 +17,8 @@ public class IncorrectCommand extends Command{
         return new CommandResult(feedbackToUser);
     }
 
+    // returns false that means data not mutated
+    public boolean isMutating() {
+        return false;
+    }
 }
--- src/seedu/addressbook/commands/ViewAllCommand.java
+++ src/seedu/addressbook/commands/ViewAllCommand.java
@@ -3,7 +3,6 @@ package seedu.addressbook.commands;
 import seedu.addressbook.common.Messages;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
-
 /**
  * Shows all details of the person identified using the last displayed index.
  * Private contact details are shown.
@@ -13,17 +12,14 @@ public class ViewAllCommand extends Command {
     public static final String COMMAND_WORD = "viewall";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" + "Shows all details of the person "
-            + "identified by the index number in the last shown person listing.\n\t"
-            + "Parameters: INDEX\n\t"
+            + "identified by the index number in the last shown person listing.\n\t" + "Parameters: INDEX\n\t"
             + "Example: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_VIEW_PERSON_DETAILS = "Viewing person: %1$s";
 
-
     public ViewAllCommand(int targetVisibleIndex) {
         super(targetVisibleIndex);
     }
-
 
     @Override
     public CommandResult execute() {
@@ -37,4 +33,10 @@ public class ViewAllCommand extends Command {
             return new CommandResult(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
     }
+
+    // returns false that means data not mutated
+    public boolean isMutating() {
+        return false;
+    }
+
 }
